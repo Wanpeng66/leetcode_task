d7c609a7b868df945e22302ffbedbeb28ef8e6a7
wp <18717792412@163.com>
2022-03-19 16:20:16 +0800

    commit

--- src/main/java/com/wp/task_547.java
+++ src/main/java/com/wp/task_547.java
@@ -1,8 +1,16 @@
 package com.wp;
 
-import java.io.UnsupportedEncodingException;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import jxl.Workbook;
+import jxl.format.Colour;
+import jxl.format.UnderlineStyle;
+import jxl.write.*;
+
+import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.*;
 
 /**
  * @author: wp
@@ -11,14 +19,94 @@ import java.security.NoSuchAlgorithmException;
  * @date 2022/3/18 11:02
  */
 public class task_547 {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
         /*String apk = "8n8K4P7N";
         String secret = "527fb3f2acc09d4323373a180d67925150761ea6";
         long timestamp = System.currentTimeMillis();
         System.out.println( timestamp );
         System.out.println( getSHA256StrJava( apk + timestamp + secret ) );*/
-        int[][] isConnected = {{1,1,0},{1,1,0},{0,0,1}};
-        System.out.println( findCircleNum( isConnected ) );
+        /*int[][] isConnected = {{1,1,0},{1,1,0},{0,0,1}};
+        System.out.println( findCircleNum( isConnected ) );*/
+        Map<String, JSONObject> map = new HashMap<>();
+        File root = new File( "C:\\Users\\wp\\Desktop\\赛迪需求数据" );
+        File[] files = root.listFiles();
+        for (File file : files) {
+            String name = file.getName();
+            String str = readJsonFile( file );
+            JSONObject jsonData = JSONObject.parseObject( str );
+            JSONArray values = jsonData.getJSONObject( "data" ).getJSONArray( "values" );
+            JSONArray datas = new JSONArray();
+            HashSet<String> cols = new HashSet<>();
+            for (int i = 0; i < values.size(); i++) {
+                JSONObject data = values.getJSONObject( i ).getJSONObject( "_source" );
+                datas.add( data );
+                cols.addAll( data.keySet() );
+            }
+            for (int i = 0; i < datas.size(); i++) {
+                JSONObject data = datas.getJSONObject( i );
+                Set<String> set = data.keySet();
+                HashSet<String> hashSet = new HashSet<>( cols );
+                hashSet.removeAll( set );
+                for (String s : hashSet) {
+                    data.put( s,null );
+                }
+            }
+            JSONObject data = new JSONObject();
+            data.put( "cols",cols );
+            data.put( "datas",datas );
+            map.put( name.replace( ".json","" ),data );
+        }
+
+        String[] array = map.keySet().toArray( new String[0] );
+        for (int i = 0; i < array.length; i++) {
+            WritableWorkbook workbook = Workbook.createWorkbook(new File( "F:\\file\\"+array[i]+".xls" ));
+            WritableSheet sheet = workbook.createSheet("sheet", 0);
+            JSONObject jsonData = map.get( array[i] );
+            List<String> cols = new ArrayList<>(jsonData.getObject( "cols", HashSet.class ));
+            JSONArray datas = jsonData.getJSONArray( "datas" );
+            for (int j = 0; j < cols.size(); j++) {
+                Label label = new Label( j,0,cols.get( j ) );
+                sheet.addCell( label );
+            }
+            int row = 1;
+            for (int j = 0; j < datas.size(); j++) {
+                for (int k = 0; k < cols.size(); k++) {
+                    JSONObject data = datas.getJSONObject( j );
+                    String key = cols.get( k );
+                    Object val = data.get( key );
+                    String str = null==val?null:val.toString();
+                    Label label = new Label( k,row,str );
+                    sheet.addCell( label );
+                }
+                row++;
+            }
+            //开始执行写入操作
+            workbook.write();
+            //关闭流
+            workbook.close();
+        }
+
+
+    }
+
+    public static String readJsonFile(File jsonFile) {
+        String jsonStr = "";
+        try {
+            FileReader fileReader = new FileReader(jsonFile);
+            Reader reader = new InputStreamReader(new FileInputStream(jsonFile),"utf-8");
+            int ch = 0;
+            StringBuffer sb = new StringBuffer();
+            while ((ch = reader.read()) != -1) {
+                sb.append((char) ch);
+            }
+            fileReader.close();
+            reader.close();
+            jsonStr = sb.toString();
+            return jsonStr;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public static int findCircleNum(int[][] isConnected) {
--- src/main/java/com/wp/task_638.java
+++ src/main/java/com/wp/task_638.java
@@ -1,5 +1,7 @@
 package com.wp;
 
+import com.sun.corba.se.impl.oa.toa.TOA;
+
 import java.util.*;
 
 /**
@@ -14,12 +16,102 @@ public class task_638 {
 
     public static void main( String[] args ) {
         List<Integer> price = Arrays.asList( 0,0,0 );
-        List<Integer> needs = Arrays.asList( 1,1,0 );
+        List<Integer> needs = Arrays.asList( 2,2,1 );
         List<List<Integer>> special = new ArrayList<>();
         special.add( Arrays.asList( 1,1,0,4 ) );
         special.add( Arrays.asList( 2,2,1,9 ) );
-        System.out.println( shoppingOffers( price, special, needs ) );
+        long l = System.currentTimeMillis();
+        int i = shoppingOffers2( price, special, needs );
+        System.out.println(System.currentTimeMillis()-l);
+        System.out.println( i );
 
+    }
+    static List<List<Integer>> policy = null;
+    public static int shoppingOffers2(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        int size = price.size();
+        //去除掉比单独买相同数量商品还贵的大礼包
+        Iterator<List<Integer>> iterator = special.iterator();
+        while (iterator.hasNext()) {
+            List<Integer> list = iterator.next();
+            long sum = 0l;
+            for (int i = 0; i < list.size()-1; i++) {
+                Integer num = list.get( i );
+                sum += price.get( i )*num;
+            }
+            if (sum<list.get( size )) {
+                iterator.remove();
+            }
+        }
+        //将单独买一件商品也转换成大礼包
+        for (int i = 0; i < size; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < size; j++) {
+                if (i==j) {
+                    list.add( j,1 );
+                }else{
+                    list.add( j,0 );
+                }
+            }
+            list.add( size,price.get( i ) );
+            special.add( list );
+        }
+        long[] total = {Long.MAX_VALUE};
+        ArrayList<Integer> cNeeds = new ArrayList();
+        Map<String,Long> map = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            cNeeds.add( i,0 );
+        }
+        shoppingOffers(special,needs,0,cNeeds,total,new ArrayList<>(),map);
+        return (int) total[0];
+    }
+
+    private static void shoppingOffers(  List<List<Integer>> special, List<Integer> needs,long currentSum,ArrayList<Integer> cNeeds,long[] total, List<List<Integer>> cPolicy,Map<String,Long> map ) {
+        boolean isDone = true;
+        for (int i = cNeeds.size() - 1; i >= 0; i--) {
+            if (cNeeds.get( i )!=needs.get( i )) {
+                isDone = false;
+                break;
+            }
+        }
+        if (isDone && total[0]>currentSum) {
+            total[0] = currentSum;
+            policy = new ArrayList<>(cPolicy);
+            return;
+        }
+        for (int i = 0; i < special.size(); i++) {
+            List<Integer> list = special.get( i );
+            Integer sum = list.get( list.size() - 1 );
+            boolean flag = true;
+            if (total[0]!=Long.MAX_VALUE && currentSum+sum>total[0]) {
+                flag = false;
+            }else{
+                for (int j = 0; j < list.size()-1; j++) {
+                    if (cNeeds.get( j )+list.get( j )>needs.get( j )) {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if (flag) {
+                for (int j = 0; j < list.size()-1; j++) {
+                    cNeeds.set( j,cNeeds.get( j )+list.get( j ) );
+                }
+                currentSum += list.get( list.size()-1 );
+                String key = cNeeds.toString();
+                if (!map.containsKey( key) || map.get( key )>currentSum) {
+                    int size = cPolicy.size();
+                    cPolicy.add( size,list );
+                    map.put( key,currentSum );
+                    shoppingOffers( special,needs,currentSum,cNeeds,total,cPolicy,map );
+                    cPolicy.remove( size );
+                }
+                for (int j = 0; j < list.size()-1; j++) {
+                    cNeeds.set( j,cNeeds.get( j )-list.get( j ) );
+                }
+                currentSum -= list.get( list.size()-1 );
+
+            }
+        }
     }
 
     static List<Integer> Price ;
